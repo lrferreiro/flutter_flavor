b3782dd97d2d5754ffc2fa55b0851d587a49f08d
Luis Reinier Ferreiro Vázquez <lrferreiro910208@gmail.com>
2020-04-06 18:03:36 -0400

    Updated the example.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,76 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flavor/flutter_flavor.dart';
 
+import 'my_app.dart';
+
 
 void main() {
     FlavorConfig(
+            environment: FlavorEnvironment.PROD,
             variables: {
-                "baseUrl": "https://www.example.com",
-            });
+                "counter": 0,
+                "baseUrl": "https://www.example1.com",
+            }
+    );
     return runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return FlavorBanner(
-            child: MaterialApp(
-                title: 'Flutter Demo',
-                theme: ThemeData(
-                    primarySwatch: Colors.blue,
-                ),
-                home: MyHomePage(title: 'Flutter Demo Home Page'),
-            ),
-        );
-    }
-}
-
-class MyHomePage extends StatefulWidget {
-    MyHomePage({Key key, this.title}) : super(key: key);
-
-    final String title;
-
-    @override
-    _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-    int _counter = 0;
-
-    void _incrementCounter() {
-        setState(() {
-            _counter++;
-        });
-    }
-
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: Text(widget.title),
-            ),
-            body: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                        Text(
-                            'You have pushed the button this many times:',
-                        ),
-                        Text(
-                            '$_counter',
-                            style: Theme
-                                    .of(context)
-                                    .textTheme
-                                    .display1,
-                        ),
-                    ],
-                ),
-            ),
-            floatingActionButton: FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: Icon(Icons.add),
-            ),
-        );
-    }
 }
