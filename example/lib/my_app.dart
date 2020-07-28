8a4698db3552c1a5929f2019d2fe28fb056d23eb
Luis Reinier Ferreiro Vázquez <lrferreiro910208@gmail.com>
2020-07-28 01:19:37 -0400

    Updated example

--- example/lib/my_app.dart
+++ example/lib/my_app.dart
@@ -2,66 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:flutter_flavor/flutter_flavor.dart';
 
 class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return FlavorBanner(
-            child: MaterialApp(
-                title: 'Flutter Demo',
-                theme: ThemeData(
-                    primarySwatch: Colors.blue,
-                ),
-                home: MyHomePage(title: 'Flutter Demo Home Page'),
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return FlavorBanner(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
-    MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key, this.title}) : super(key: key);
 
-    final String title;
+  final String title;
 
-    @override
-    _MyHomePageState createState() => _MyHomePageState();
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-    int _counter = FlavorConfig.instance.variables["counter"];
+  int _counter = FlavorConfig.instance.variables["counter"];
 
-    void _incrementCounter() {
-        setState(() {
-            _counter++;
-        });
-    }
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: Text(widget.title),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
             ),
-            body: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                        Text(
-                            'You have pushed the button this many times:',
-                        ),
-                        Text(
-                            '$_counter',
-                            style: Theme
-                                    .of(context)
-                                    .textTheme
-                                    .display1,
-                        ),
-                    ],
-                ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
             ),
-            floatingActionButton: FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: Icon(Icons.add),
-            ),
-        );
-    }
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
 }
--- test/flutter_flavor_test.dart
+++ test/flutter_flavor_test.dart
@@ -1,6 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_flavor/flutter_flavor.dart';
-
-void main() {
-}
+void main() {}
